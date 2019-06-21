3816d3803b4f2e12deccb352c4aabe9cface85a7
Caleb Klingler <cjkling6@gmail.com>
2019-06-20 20:08:00 -0500

    Finished rendering form and added welcome page

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
+def index():
+    return render_template('index.html', title='Signup')
+
+
+@app.route('/welcome', methods=['POST'])
+def welcome():
+    username=request.form['username']
+    return render_template('welcome.html', title='Welcome',username=username )
+
+app.run()
