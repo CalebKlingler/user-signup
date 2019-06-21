7dd52b5b6a8b90d60b697f88f793e62fddf47e7d
Caleb Klingler <cjkling6@gmail.com>
2019-06-20 21:49:10 -0500

    Got username validation working

--- main.py
+++ main.py
@@ -7,12 +7,27 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Signup')
+    username=''
+    password=''
+    verpassword=''
+    email=''
+    return render_template('index.html', title='Signup', username=username, password=password, verpassword=verpassword, email=email)
 
 
 @app.route('/welcome', methods=['POST'])
 def welcome():
     username=request.form['username']
-    return render_template('welcome.html', title='Welcome',username=username )
+    password=request.form['password']
+    verpassword=request.form['ver-password']
+    email=request.form['email']
+    if username=='' or len(username)<3 or len(username) > 20:
+        return render_template('index.html', usererror='y', username=username)
+    else:
+        for char in username:
+            if char==' ':
+                return render_template('index.html', usererror='y', username=username, email=email)
+        return render_template('welcome.html', title='Welcome',username=username)
+    if password=='' or len(password)<3 or len(password)>20:
+        return render_template('index.html', passerror='y', username=username, email=email)
 
 app.run()
